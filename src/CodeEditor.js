7972474fdd14daa213fd527ddd9986bafae03514
Canyon Smith <canyonfsmith@gmail.com>
2023-10-03 10:26:43 -0600

    updated src/CodeEditor.js

--- src/CodeEditor.js
+++ src/CodeEditor.js
@@ -2,39 +2,47 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import Modal from './Modal';
 
-const Button = ({ onClick, children, active }) => (
-  <button 
-    onClick={onClick} 
-    className={`px-4 py-2 font-bold text-gray-600 bg-gray-200 hover:bg-gray-300 rounded-t-lg ${active ? 'bg-gray-300' : ''}`}
-  >
-    {children}
-  </button>
-);
+const Button = ({ onClick, children, active }) => {
+  const buttonClass = `px-4 py-2 font-bold text-gray-600 bg-gray-200 hover:bg-gray-300 rounded-t-lg ${active ? 'bg-gray-300' : ''}`;
+  return (
+    <button onClick={onClick} className={buttonClass}>
+      {children}
+    </button>
+  );
+};
 
-const Select = ({ value, onChange, options }) => (
-  <select className="w-full mr-2 py-2 px-4 rounded shadow-lg" value={value} onChange={onChange}>
-    {options.map(option => <option key={option.value} value={option.value}>{option.label}</option>)}
-  </select>
-);
+const Select = ({ value, onChange, options }) => {
+  const selectOptions = options.map(option => <option key={option.value} value={option.value}>{option.label}</option>);
+  return (
+    <select className="w-full mr-2 py-2 px-4 rounded shadow-lg" value={value} onChange={onChange}>
+      {selectOptions}
+    </select>
+  );
+};
 
-const CodeEditor = ({
-  modalVisible,
-  setModalVisible,
-  repos,
-  selectedRepo,
-  handleRepoChange,
-  files,
-  selectedFile,
-  handleFileChange,
-  code,
-  output,
-  view,
-  handleViewChange,
-  handleRunCode,
-  handleSaveCode,
-  textareaValue,
-  setTextareaValue,
-}) => {
+const CodeEditor = (props) => {
+  const {
+    modalVisible,
+    setModalVisible,
+    repos,
+    selectedRepo,
+    handleRepoChange,
+    files,
+    selectedFile,
+    handleFileChange,
+    code,
+    output,
+    view,
+    handleViewChange,
+    handleRunCode,
+    handleSaveCode,
+    textareaValue,
+    setTextareaValue,
+  } = props;
+
+  const repoOptions = repos.map(repo => ({ value: repo, label: repo }));
+  const fileOptions = files.map(file => ({ value: file.download_url, label: file.name }));
+
   return (
     <div className="container mx-auto p-4">
       <div className="flex justify-start mb-4">
@@ -42,8 +50,8 @@ const CodeEditor = ({
         <Button onClick={() => handleViewChange('code')} active={view === 'code'}><i data-feather="code"></i></Button>
       </div>
       <div className="flex justify-between mb-4">
-        <Select value={selectedRepo} onChange={handleRepoChange} options={repos.map(repo => ({ value: repo, label: repo }))} />
-        <Select value={selectedFile} onChange={handleFileChange} options={files.map(file => ({ value: file.download_url, label: file.name }))} />
+        <Select value={selectedRepo} onChange={handleRepoChange} options={repoOptions} />
+        <Select value={selectedFile} onChange={handleFileChange} options={fileOptions} />
       </div>
       <div className="flex flex-col h-screen">
         <div className="flex-grow">
