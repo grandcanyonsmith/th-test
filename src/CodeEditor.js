0fff99df6a60413c2abe6e6c9004971138402f7c
Canyon Smith <canyonfsmith@gmail.com>
2023-10-03 10:53:50 -0600

    Update CodeEditor.js

--- src/CodeEditor.js
+++ src/CodeEditor.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import Modal from './Modal';
+import ReferenceFileSelector from './ReferenceFileSelector';
 
 const Button = ({ onClick, children, active }) => {
   const buttonClass = `px-4 py-2 font-bold text-white bg-gray-800 hover:bg-gray-700 rounded-t-lg ${active ? 'bg-gray-700' : ''}`;
@@ -22,73 +22,73 @@ const Select = ({ value, onChange, options }) => {
 
 const CodeEditor = (props) => {
   const {
-    modalVisible,
-    setModalVisible,
-    repos,
-    selectedRepo,
-    handleRepoChange,
-    files,
-    selectedFile,
-    handleFileChange,
-    code,
-    output,
-    view,
-    handleViewChange,
-    handleRunCode,
-    handleSaveCode,
-    textareaValue,
-    setTextareaValue,
+    isReferenceFileSelectorVisible,
+    setReferenceFileSelectorVisible,
+    githubRepos,
+    selectedGithubRepo,
+    handleGithubRepoChange,
+    githubRepoFiles,
+    selectedGithubRepoFile,
+    handleGithubRepoFileChange,
+    sourceCode,
+    codePreview,
+    activeView,
+    handleActiveViewChange,
+    handleUserRequest,
+    handleSaveSourceCode,
+    userRequest,
+    setUserRequest,
   } = props;
 
-  const repoOptions = repos.map(repo => ({ value: repo, label: repo }));
-  const fileOptions = files.map(file => ({ value: file.download_url, label: file.name }));
+  const repoOptions = githubRepos.map(repo => ({ value: repo, label: repo }));
+  const fileOptions = githubRepoFiles.map(file => ({ value: file.download_url, label: file.name }));
 
   return (
     <div className="container mx-auto p-4 bg-gray-900 text-white">
       <div className="flex justify-start mb-4">
-        <Button onClick={() => handleViewChange('output')} active={view === 'output'}><i data-feather="eye"></i></Button>
-        <Button onClick={() => handleViewChange('code')} active={view === 'code'}><i data-feather="code"></i></Button>
+        <Button onClick={() => handleActiveViewChange('output')} active={activeView === 'output'}><i data-feather="eye"></i></Button>
+        <Button onClick={() => handleActiveViewChange('code')} active={activeView === 'code'}><i data-feather="code"></i></Button>
       </div>
       <div className="flex justify-between mb-4">
-        <Select value={selectedRepo} onChange={handleRepoChange} options={repoOptions} />
-        <Select value={selectedFile} onChange={handleFileChange} options={fileOptions} />
+        <Select value={selectedGithubRepo} onChange={handleGithubRepoChange} options={repoOptions} />
+        <Select value={selectedGithubRepoFile} onChange={handleGithubRepoFileChange} options={fileOptions} />
       </div>
       <div className="flex flex-col h-screen">
         <div className="flex-grow">
-          {view === 'code' ? (
-            <pre className="bg-gray-800 rounded shadow-lg p-4 language-html">{code}</pre>
+          {activeView === 'code' ? (
+            <pre className="bg-gray-800 rounded shadow-lg p-4 language-html">{sourceCode}</pre>
           ) : (
             <iframe 
               title="Code Output"
-              key={code} 
+              key={sourceCode} 
               className="bg-gray-800 rounded shadow-lg p-4" 
-              srcDoc={output} 
+              srcDoc={codePreview} 
               style={{height: '100vh', width: '100%'}}
             />
           )}
         </div>
         <textarea
-          value={textareaValue}
-          onChange={e => setTextareaValue(e.target.value)}
+          value={userRequest}
+          onChange={e => setUserRequest(e.target.value)}
           className="bg-gray-800 text-white"
         />
       </div>
       <div className="flex justify-between mt-4">
         <div className="flex flex-col justify-between h-full">
-          <Button onClick={() => setModalVisible(true)}><i data-feather="plus" className="h-4 w-4"></i></Button>
-          <Button onClick={handleRunCode}><i data-feather="send" className="h-4 w-4"></i></Button>
-          <Button onClick={handleSaveCode}><i data-feather="save" className="h-4 w-4"></i></Button>
+          <Button onClick={() => setReferenceFileSelectorVisible(true)}><i data-feather="plus" className="h-4 w-4"></i></Button>
+          <Button onClick={handleUserRequest}><i data-feather="send" className="h-4 w-4"></i></Button>
+          <Button onClick={handleSaveSourceCode}><i data-feather="save" className="h-4 w-4"></i></Button>
         </div>
       </div>
-      {modalVisible && (
-        <Modal 
-          repos={repos} 
-          selectedRepo={selectedRepo} 
-          handleRepoChange={handleRepoChange} 
-          files={files} 
-          selectedFile={selectedFile}
-          handleFileChange={handleFileChange} 
-          closeModal={() => setModalVisible(false)}
+      {isReferenceFileSelectorVisible && (
+        <ReferenceFileSelector 
+          githubRepos={githubRepos} 
+          selectedGithubRepo={selectedGithubRepo} 
+          handleGithubRepoChange={handleGithubRepoChange} 
+          githubRepoFiles={githubRepoFiles} 
+          selectedGithubRepoFile={selectedGithubRepoFile}
+          handleGithubRepoFileChange={handleGithubRepoFileChange} 
+          closeReferenceFileSelector={() => setReferenceFileSelectorVisible(false)}
         />
       )}
     </div>
@@ -96,22 +96,22 @@ const CodeEditor = (props) => {
 };
 
 CodeEditor.propTypes = {
-  modalVisible: PropTypes.bool.isRequired,
-  setModalVisible: PropTypes.func.isRequired,
-  repos: PropTypes.array.isRequired,
-  selectedRepo: PropTypes.string.isRequired,
-  handleRepoChange: PropTypes.func.isRequired,
-  files: PropTypes.array.isRequired,
-  selectedFile: PropTypes.string.isRequired,
-  handleFileChange: PropTypes.func.isRequired,
-  code: PropTypes.string.isRequired,
-  output: PropTypes.string.isRequired,
-  view: PropTypes.string.isRequired,
-  handleViewChange: PropTypes.func.isRequired,
-  handleRunCode: PropTypes.func.isRequired,
-  handleSaveCode: PropTypes.func.isRequired,
-  textareaValue: PropTypes.string.isRequired,
-  setTextareaValue: PropTypes.func.isRequired,
+  isReferenceFileSelectorVisible: PropTypes.bool.isRequired,
+  setReferenceFileSelectorVisible: PropTypes.func.isRequired,
+  githubRepos: PropTypes.array.isRequired,
+  selectedGithubRepo: PropTypes.string.isRequired,
+  handleGithubRepoChange: PropTypes.func.isRequired,
+  githubRepoFiles: PropTypes.array.isRequired,
+  selectedGithubRepoFile: PropTypes.string.isRequired,
+  handleGithubRepoFileChange: PropTypes.func.isRequired,
+  sourceCode: PropTypes.string.isRequired,
+  codePreview: PropTypes.string.isRequired,
+  activeView: PropTypes.string.isRequired,
+  handleActiveViewChange: PropTypes.func.isRequired,
+  handleUserRequest: PropTypes.func.isRequired,
+  handleSaveSourceCode: PropTypes.func.isRequired,
+  userRequest: PropTypes.string.isRequired,
+  setUserRequest: PropTypes.func.isRequired,
 };
 
 export default CodeEditor;
