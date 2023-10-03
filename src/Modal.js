32a2ea5ca3a47fa02c920eb20b3fb97da5175873
Canyon Smith <canyonfsmith@gmail.com>
2023-10-03 10:55:47 -0600

    Update Modal.js

--- src/Modal.js
+++ src/Modal.js
@@ -1,21 +1,21 @@
 import React from 'react';
 
-const Modal = ({ repos, files, isOpen, closeModal }) => {
-  console.log(files); // This will log the files prop to the console
+const ReferenceFileSelector = ({ githubRepos, githubRepoFiles, isReferenceFileSelectorOpen, closeReferenceFileSelector }) => {
+  console.log(githubRepoFiles); // This will log the githubRepoFiles prop to the console
 
-  const [selectedRepo, setSelectedRepo] = React.useState(repos && repos.length > 0 ? repos[0] : '');
-  const [selectedFile, setSelectedFile] = React.useState(files && files.length > 0 ? files[0] : null);
+  const [selectedGithubRepo, setSelectedGithubRepo] = React.useState(githubRepos && githubRepos.length > 0 ? githubRepos[0] : '');
+  const [selectedGithubRepoFile, setSelectedGithubRepoFile] = React.useState(githubRepoFiles && githubRepoFiles.length > 0 ? githubRepoFiles[0] : null);
 
-  const handleRepoChange = (e) => {
-    setSelectedRepo(e.target.value);
+  const handleGithubRepoSelection = (e) => {
+    setSelectedGithubRepo(e.target.value);
   };
 
-  const handleFileChange = (e) => {
-    const file = files.find(file => file.download_url === e.target.value);
-    setSelectedFile(file);
+  const handleGithubRepoFileSelection = (e) => {
+    const file = githubRepoFiles.find(file => file.download_url === e.target.value);
+    setSelectedGithubRepoFile(file);
   };
 
-  if (!isOpen) {
+  if (!isReferenceFileSelectorOpen) {
     return null;
   }
 
@@ -34,18 +34,18 @@ const Modal = ({ repos, files, isOpen, closeModal }) => {
                   Select a Repository and a File
                 </h3>
                 <div className="mt-2">
-                  <select className="w-full py-2 px-4 rounded shadow-lg" value={selectedRepo} onChange={handleRepoChange}>
-                    {repos && repos.map(repo => <option key={repo} value={repo}>{repo}</option>)}
+                  <select className="w-full py-2 px-4 rounded shadow-lg" value={selectedGithubRepo} onChange={handleGithubRepoSelection}>
+                    {githubRepos && githubRepos.map(repo => <option key={repo} value={repo}>{repo}</option>)}
                   </select>
-                  <select className="w-full mt-2 py-2 px-4 rounded shadow-lg" value={selectedFile ? selectedFile.download_url : ''} onChange={handleFileChange}>
-                    {files && files.map(file => <option key={file.download_url} value={file.download_url}>{file.name}</option>)}
+                  <select className="w-full mt-2 py-2 px-4 rounded shadow-lg" value={selectedGithubRepoFile ? selectedGithubRepoFile.download_url : ''} onChange={handleGithubRepoFileSelection}>
+                    {githubRepoFiles && githubRepoFiles.map(file => <option key={file.download_url} value={file.download_url}>{file.name}</option>)}
                   </select>
                 </div>
               </div>
             </div>
           </div>
           <div className="bg-gray-50 px-4 py-3 sm:px-6 sm:flex sm:flex-row-reverse">
-            <button type="button" className="w-full inline-flex justify-center rounded-md border border-transparent shadow-sm px-4 py-2 bg-blue-600 text-base font-medium text-white hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-500 sm:ml-3 sm:w-auto sm:text-sm" onClick={closeModal}>
+            <button type="button" className="w-full inline-flex justify-center rounded-md border border-transparent shadow-sm px-4 py-2 bg-blue-600 text-base font-medium text-white hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-500 sm:ml-3 sm:w-auto sm:text-sm" onClick={closeReferenceFileSelector}>
               Close
             </button>
           </div>
@@ -55,4 +55,4 @@ const Modal = ({ repos, files, isOpen, closeModal }) => {
   );
 };
 
-export default Modal;
+export default ReferenceFileSelector;
